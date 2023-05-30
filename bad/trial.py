d4bff32bf21af64cda04d379b5bc136f6cf64fb0
helloworld1327 <132551749+helloworld1327@users.noreply.github.com>
2023-05-30 19:27:21 +0530

    Update trial.py

--- bad/trial.py
+++ bad/trial.py
@@ -1,1 +1,10 @@
 a = HYPERLINK(link_location, [friendly_name])
+
+from flask import request, render_template_string
+
+# /hello?username={{config}} will display the entire flask configuration and potential secrets
+@app.route('/hello')
+def hello():
+    username = request.args.get('username')
+    template = f"<p>Hello {username}</p>" # User input is used directly in the string to be rendered
+    return render_template_string(template) # Noncompliant
