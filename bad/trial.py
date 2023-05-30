622c8e3d64752c88a0fe2cc7f5af30cb9c43cf41
helloworld1327 <132551749+helloworld1327@users.noreply.github.com>
2023-05-30 19:34:27 +0530

    Update trial.py

--- bad/trial.py
+++ bad/trial.py
@@ -7,5 +7,5 @@ from flask import request, render_template_string
 @app.route('/hello')
 def hello():
     username = request.args.get('username')
-    template = f"<p>Hello {username}</p>" # User input is used directly in the string to be rendered
-    return render_template_string(template) # Noncompliant
+    template = f"<p>Hello {nameplease}</p>"
+    return render_template_string(template)
